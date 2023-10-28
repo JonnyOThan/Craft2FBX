03fc6d186bec87a055a4755b705672c4db776de3
JonnyOThan <jonnyothan@gmail.com>
2023-10-28 08:49:25 -0400

    hook up toolbar button

--- Source/Craft2FBXAddon.cs
+++ Source/Craft2FBXAddon.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KSP.UI.Screens;
+using System;
+using ToolbarControl_NS;
+using UnityEngine;
 
 namespace Craft2FBX
 {
-    [KSPAddon(KSPAddon.Startup.FlightAndEditor, false)]
-    public class Craft2FBXAddon
-    {
-    }
+	[KSPAddon(KSPAddon.Startup.FlightAndEditor, false)]
+	public class Craft2FBXAddon : MonoBehaviour
+	{
+		ApplicationLauncherButton toolbarButton;
+
+		void Start()
+		{
+			var iconTexture = GameDatabase.Instance.GetTexture("Craft2FBX/icon", false);
+			toolbarButton = ApplicationLauncher.Instance.AddModApplication(ToolbarClick, ToolbarClick, null, null, null, null, ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.VAB | ApplicationLauncher.AppScenes.SPH, iconTexture);
+		}
+
+		void OnDestroy()
+		{
+			ApplicationLauncher.Instance.RemoveModApplication(toolbarButton);
+		}
+
+		private void ToolbarClick()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
